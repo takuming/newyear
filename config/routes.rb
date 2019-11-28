476910752abcce6dfe2db3c2899eb6a2eeac6b98
カイクン <takumi.kai.skywalker@gmail.com>
2019-11-29 08:43:46 +0900

    text

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+  ActiveAdmin.routes(self)
   devise_for :users
   root to: 'page#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # APIコントローラへのルーティング
+  namespace :api, {format: 'json'} do
+    namespace :v1 do
+      resources :employees, only: [:index, :show]
+    end
+  end
+  
 end
